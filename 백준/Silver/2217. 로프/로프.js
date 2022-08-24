76cb4ec8d430a596c75bd81ea4b87e6b83652cf9
oobinss <82925631+oobinss@users.noreply.github.com>
2022-08-24 21:53:59 +0900

    [Silver IV] Title: 로프, Time: 272 ms, Memory: 26276 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.js
+++ 백준/Silver/2217. 로프/로프.js
@@ -4,6 +4,6 @@ ropes.sort((a, b) => a-b);
 let min = ropes[0]*n;
 for (let i=1; i<n; i++) {
     weight = ropes[i]*(n-i);
-    min < weight ? min = weight : min;
+    min = (min < weight ? weight : min);
 }
 console.log(min);
