26b45c2d9d2bca23eb7cee78d4010a713d0d0e0d
oobinss <82925631+oobinss@users.noreply.github.com>
2022-08-30 22:28:13 +0900

    [Silver IV] Title: GCD 합, Time: 204 ms, Memory: 10972 KB -BaekjoonHub

---
+++ 백준/Silver/9613. GCD 합/GCD 합.js
@@ -0,0 +1,33 @@
+const rl = require('readline').createInterface(process.stdin, process.stdout);
+
+let input = [];
+
+rl.on('line', (line) => {
+  input.push(line.split(' ').map((x) => parseInt(x)));
+}).on('close', () => {
+  const testCase = +input.shift();
+  let answer = '';
+
+  for (let i = 0; i < testCase; i++) {
+    answer += calc(input[i][0], input[i].splice(1)) + '\n';
+  }
+
+  console.log(answer);
+});
+
+function calc(a, b) {
+  let sum = 0;
+
+  for (let i = 0; i < a - 1; i++)
+    for (let j = i + 1; j < a; j++) sum += gcd(b[i], b[j]);
+
+  return sum;
+}
+
+function gcd(x, y) {
+  while (y) {
+    [x, y] = [y, x % y];
+  }
+
+  return x;
+}
