6f8409c8bdb1df3bb293fe839ae34b52358bfb01
oobinss <82925631+oobinss@users.noreply.github.com>
2025-08-18 21:22:47 +0900

    [Bronze I] Title: FizzBuzz, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
@@ -0,0 +1,14 @@
+for i in range(3, 0, -1):
+    x = input()
+    if x not in ['Fizz', 'Buzz', 'FizzBuzz']:
+        n = int(x) + i
+        break
+
+if n % 15 == 0:
+    print('FizzBuzz')
+elif n % 3 == 0:
+    print('Fizz')
+elif n % 5 == 0:
+    print('Buzz')
+else:
+    print(n)
