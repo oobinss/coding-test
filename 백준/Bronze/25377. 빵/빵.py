1228f29532cc65612d79ea4dbe031fb5dcfc0723
oobinss <82925631+oobinss@users.noreply.github.com>
2025-08-21 19:01:54 +0900

    [Bronze IV] Title: 빵, Time: 44 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -1,11 +1,9 @@
 n = int(input())
-time = 999
+ans = float('inf') 
+
 for _ in range(n):
-    a,b = map(int, input().split())
+    a, b = map(int, input().split())
     if a <= b:
-        if b < time:
-            time = b
-           
-if time == 999:
-    time = -1
-print(time)            
+        ans = min(ans, b)
+
+print(ans if ans != float('inf') else -1)
