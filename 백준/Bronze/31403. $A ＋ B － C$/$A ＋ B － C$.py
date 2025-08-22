aeea8afdaa5dc668ee789e07465f6d926f3857ef
oobinss <82925631+oobinss@users.noreply.github.com>
2025-08-22 22:25:23 +0900

    [Bronze IV] Title: $A + B - C$, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
@@ -0,0 +1,12 @@
+A = input().strip()
+B = input().strip()
+C = input().strip()
+
+int_result = int(A) + int(B) - int(C)
+    
+    
+str_concat = A + B
+str_result = int(str_concat) - int(C)
+    
+print(int_result)
+print(str_result)
