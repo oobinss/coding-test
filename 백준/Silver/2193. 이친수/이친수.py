6077a38047c7614712eea8d7a484702d8f50afc9
oobinss <82925631+oobinss@users.noreply.github.com>
2025-09-21 22:02:48 +0900

    [Silver III] Title: 이친수, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,15 @@
+def pinary_count(N):
+    if N == 1:
+        return 1
+
+    dp = [[0, 0] for _ in range(N + 1)]
+    dp[1][1] = 1
+
+    for i in range(2, N + 1):
+        dp[i][0] = dp[i-1][0] + dp[i-1][1]
+        dp[i][1] = dp[i-1][0]
+
+    return dp[N][0] + dp[N][1]
+
+N = int(input())
+print(pinary_count(N))
