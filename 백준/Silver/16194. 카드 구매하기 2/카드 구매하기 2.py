20446bcb26f12cefa658b9e85c67bbeffaba2188
oobinss <82925631+oobinss@users.noreply.github.com>
2025-11-10 21:42:16 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 172 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,17 @@
+n = int(input())
+cost = list(map(int, input().split()))
+
+dp = [0] + [float('inf')] * n
+
+for i in range(1, n + 1):
+    for j in range(1, i+1):
+        dp[i] = min(dp[i], dp[i- j] + cost[j-1])
+        
+
+print(dp[n])
+
+        
+        
+            
+        
+    
