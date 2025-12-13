35078cd087b0ac2a6faf7255f3e6136628724dac
oobinss <82925631+oobinss@users.noreply.github.com>
2025-12-13 23:37:27 +0900

    [Bronze III] Title: 윷놀이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,14 @@
+for _ in range(3):
+    arr = list(map(int, input().split()))
+    zero_count = arr.count(0)
+    
+    if zero_count == 1:
+        print("A")
+    elif zero_count == 2:
+        print("B")
+    elif zero_count == 3:
+        print("C")
+    elif zero_count == 4:
+        print("D")
+    else:
+        print("E")
